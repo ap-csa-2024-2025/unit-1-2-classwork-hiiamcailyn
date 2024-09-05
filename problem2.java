f991dc7efe6c09d43a0a3d16e775389ff225aa97
hiiamcailyn <cailyntran@gmail.com>
2024-09-05 10:10:05 -0700

    space

--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args )
+    { 
+        String name1 = "Ally";
+        String name2 = "Brandon";
+        String name3 = "Carlos";
+        String name4 = "Duke";
+        System.out.print(name4 + " " + name2 + " " + name3 + " " + name1);
+    }
+}
