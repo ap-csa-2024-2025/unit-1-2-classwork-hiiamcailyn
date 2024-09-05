97a5d6607050a38ad2f1bb341d0afc0038a4de7d
hiiamcailyn <cailyntran@gmail.com>
2024-09-05 10:13:58 -0700

    yes

--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args )
+    { 
+        String name = "Cailyn";
+        String state = "CA";
+        System.out.print("My name is " + name + ". I live in " + state + ".");
+    }
+}
